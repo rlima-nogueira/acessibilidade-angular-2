0d487b390f5f562b7e9b88a2bbce16eb03e4b34a
Rafaela <rn-15@hotmail.com>
2021-08-24 15:18:11 -0300

    Adicionando a biblioteca browseranimations e criando animações com o angular

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { fade } from '../../animations/fade';
 import { ModalConfig } from './interfaces/modal-config';
 
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+  styleUrls: ['./modal.component.scss'],
+  animations: [fade]
 })
 export class ModalComponent  {
+
+  @HostBinding('@fade') fade = true;
   public config: ModalConfig;
 
 
