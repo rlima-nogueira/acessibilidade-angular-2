bb01930186e2d4d312bd9fbdf3df74b1e7bfa8e0
Rafaela <rn-15@hotmail.com>
2021-08-25 13:55:40 -0300

    Validação de form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,53 @@
-import { ViewChild } from '@angular/core';
+import { HostBinding, OnInit, ViewChild } from '@angular/core';
 import { TemplateRef } from '@angular/core';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { fade } from './shared/animations/fade';
 import { ModalRef } from './shared/components/modal/models/modal-ref';
 import { ModalService } from './shared/components/modal/services/modal.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [fade]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   @ViewChild('modal') public modalTemplateRef: TemplateRef<any>;
   public title = 'Acessibilidade Angular';
   public firstName = 'Rafaela';
   public modalRef: ModalRef;
   public infor = false;
 
+  public form: FormGroup;
+
   constructor(
-    private modalService: ModalService
+    private modalService: ModalService,
+    private formBuilder: FormBuilder
   ) {}
+
+  public ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      firstName: ['Rafaela', Validators.required],
+      surname: ['', Validators.required],
+      age: ['', Validators.required],
+      info: [false],
+    });
+  }
 
   public showModal(): void {
     this.modalRef = this.modalService.open({
       templateRef: this.modalTemplateRef,
       title: 'User Details'
     });
+  }
 
+  public submit(): void {
+    if (this.form.invalid) {
+      return;
+    }
+    console.log(this.form.value);
+    this.modalRef.close();
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ModalModule } from './shared/components/modal/modal.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { ModalModule } from './shared/components/modal/modal.module';
     BrowserAnimationsModule,
     AppRoutingModule,
     ModalModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
